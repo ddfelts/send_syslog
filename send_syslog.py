d20f29b3175e14cadf0f92773377123302658133
andywalden <github@krakencodes.com>
2016-02-04 22:02:16 -0500

    1.0 Version

--- send_syslog.py
+++ send_syslog.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python
+
 import os
 import socket
 import zipfile
+import gzip
 import argparse
-import glob
+import datetime
+import tempfile
+import shutil
+import errno
+from glob import glob
 
-version = 0.7
+version = 1.0
 
 p = argparse.ArgumentParser(__file__,
                                  description="A syslog message generator")
@@ -20,29 +27,72 @@ p.add_argument("--port",
                     default=514,
                     help="Destination syslog port")
 
-p.add_argument("--log",
-                    "-l",
+p.add_argument("--file",
+                    "-f",
                     required=True,
-                    help="Log filename or directory of files. May be compressed.")
+                    help="Name(s) of log files. (log.txt, *.log, log*.zip")
 
-def send_logs():
-    tlogcount = 0 
-    for logfile in glob.glob(args.log):
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.connect((args.server,args.port))
-    
-        with open(logfile, 'r') as openlog:
-            logcount = 0
-            for log in openlog:
-                dlog = log.encode()
-                s.sendall(dlog)
-                logcount += 1
-                tlogcount += 1
+
+def open_socket(server, port):
+    '''Open TCP socket using supplied server IP and port. Returns socket.'''
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        s.connect((server,port))
+    except socket.error as e:
+        if s:
             s.close()
-            print ("File: ", logfile, "Logs Sent: ", logcount)
-    print ("Total Logs Sent: ", tlogcount)
+        print ("Could not open socket to", server, port)
+        print (e)
+    return s
+ 
+def check_zip(file):
+    '''If zip, unzips file into temp directory. Returns path.'''
+    if zipfile.is_zipfile(file):
+        path = os.path.dirname(os.path.realpath(file))
+        tmpdir = tempfile.mkdtemp(dir=path)
+        with zipfile.ZipFile(file, 'r') as zfile:
+            filecount = len(zfile.infolist())
+            if filecount > 1:
+                print ("Only one file per zip is supported.\nSkipping:", file)
+                try:
+                    shutil.rmtree(tmpdir)
+                except:
+                    print ("Error: Temp directory not deleted:", tmpdir)
+                return 
+            else:
+                tmpfile = ''.join(zfile.namelist())
+                zfile.extractall(tmpdir)
+                logfile = os.path.join(tmpdir, tmpfile)
+                return logfile
+    else:
+        return file
+
+def send_log(logfile):
+    '''Iterates lines from log file to an open socket. Returns lines sent'''
+    with open(logfile, 'r') as openlog:
+        logcount = 0
+        for log in openlog:
+            s.sendall(log.encode())
+            logcount += 1
+    return logcount
+
             
 if __name__ == "__main__":
     args = p.parse_args()
-    send_logs();
+    s = open_socket(args.server, args.port)
+    total = 0
+    for file in glob(args.file): 
+        logfile = check_zip(file)
+        if logfile:
+            logcount = send_log(logfile)
+            if (os.path.dirname(logfile)):
+                deldir = (os.path.dirname(logfile))
+                try:
+                    shutil.rmtree(deldir)
+                except:
+                    print ("Error: Temp directory not deleted:", tmpdir)
+            print ("Sent", logcount, "logs from", logfile)
+            total += logcount
+    s.close
+    print ("Sent", total, "logs total.")
